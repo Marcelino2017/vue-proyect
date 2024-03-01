4f677897fa40b6803bc1efe5843d1201e13b4937
Marcelino <marcelinomebe@gmail.com>
2024-03-01 17:19:00 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import ButtonCounter from './components/ButtonCounter.vue'
+</script>
+
+<template>
+  <div class="container" id="app">
+    <h1>Hola Mundo</h1>
+    <ButtonCounter />
+    <button-counter />
+  </div>
+</template>
---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+const increment = () => {
+  counter.value++
+}
+
+</script>
+
+<template>
+    <div>
+        <button @click="increment">Clicked {{ counter }} times</button>
+    </div>
+</template>
