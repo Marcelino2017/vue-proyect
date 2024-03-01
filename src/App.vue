7c5b50521f3b988d226de80b515dca784a297a92
Marcelino <marcelinomebe@gmail.com>
2024-03-01 18:06:30 -0500

    v-for en componente

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <script setup>
 import ButtonCounter from './components/ButtonCounter.vue'
 import BlogPost from './components/BlogPost.vue'
+
+const posts = [
+  { id: 1, title: 'Post 1', body: 'Incididunt ea id sunt consequat 1' },
+  { id: 2, title: 'Post 2', body: 'Incididunt ea id sunt consequat 2' },
+  { id: 3, title: 'Post 3' }
+]
+
 </script>
 
 <template>
 <div class="container" id="app">
-    <h1>Hola Mundo</h1>
+    <h1>Vue 3</h1>
+    <h2>Mis post favoritos</h2>
+
     <br>
     <ButtonCounter />
     <button-counter />
     <br>
+
     <BlogPost 
+      v-for="post in posts" 
+      :key="post.id" 
+      :title="post.title" 
+      :id="post.id" 
+      :body="post.body"
+    />
+
+    <!-- <BlogPost 
       title="Post" 
       :id="1" 
       body="Incididunt ea id sunt consequat 1"
@@ -25,7 +43,7 @@ import BlogPost from './components/BlogPost.vue'
     <BlogPost 
       title="Post" 
       :id="3"  
-    />
+    /> -->
 
 </div>
 </template>
