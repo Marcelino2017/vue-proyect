344235f48089da7eda3819161925622bfeb8e4b2
Marcelino <marcelinomebe@gmail.com>
2024-03-03 15:54:29 -0500

    Realizando la funcionalidad del Loading

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import { ref, computed } from 'vue'
 import ButtonCounter from './components/ButtonCounter.vue'
 import BlogPost from './components/BlogPost.vue'
 import PaginatePost from './components/PaginatePost.vue'
+import LoadingSpinner from './components/LoadingSpinner.vue'
 
 const posts = ref([])
 const postsXpage = 10
 const inicio = ref(0)
 const fin = ref(postsXpage)
+const loading = ref(true)
 
 const favorito = ref('')
 
@@ -33,12 +35,17 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     posts.value = data
     console.log(posts.value.length)
   })
+  .catch(error => console.error(error))
+  .finally(() => {
+    loading.value = false
+  })
 
 const maxLengthPosts = computed(() => posts.value.length)
 </script>
 
-<template>
-  <div class="container-xl" id="app">
+<template class="d-flex flex-column justify-content-center aling-items-center">
+  <LoadingSpinner v-if="loading" />
+  <div class="container-xl d-flex flex-column justify-content-center aling-items-center" id="app">
       <h1>Vue 3</h1>
       <br>
       <h2>Mis post favoritos: {{ favorito }} </h2>
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+<template>
+    <div class="d-flex flex-column justify-content-center aling-items-center mt-5">
+        <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+        <p class="text-center mt-2">Cargando...</p>
+    </div>
+</template>
